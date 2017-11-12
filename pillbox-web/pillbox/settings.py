f2e72cf1a2a23001430025aa8e030585e6bcbf89
Zheng Zhang <zzjohnchn@gmail.com>
2017-11-12 18:17:49 -0500

    MySQL to SQLite

--- pillbox-web/pillbox/settings.py
+++ pillbox-web/pillbox/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'pillbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'PillboxData',
-        'USER': 'zheng',
-        'PASSWORD': '1228',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
