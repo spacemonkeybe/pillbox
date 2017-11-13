ccfae630bf15d89bee6379ac70c4a88d576e1470
yichiz1 <yichiz1@andrew.cmu>
2017-11-13 14:18:29 -0500

    change admin

--- pillbox-web/timeset/admin.py
+++ pillbox-web/timeset/admin.py
@@ -4,6 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient
+from .models import Patient, Medicine
 
+#class PatientAdmin(admin.ModelAdmin):
+#	list_display = ('patient_name')
+	
+class MedicineAdmin(admin.ModelAdmin):
+	list_display = ('patient', 'medicine_text', 'time')
+	
 admin.site.register(Patient)
+admin.site.register(Medicine, MedicineAdmin)
+
